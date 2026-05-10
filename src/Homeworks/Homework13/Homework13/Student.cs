5eaa1ca7fc22ce93d2b368f495227021da2b8911
chupchushka <chupchushka@DESKTOP-4JVCVH5>
2026-05-10 23:04:35 +0200

    Homework13

---
+++ src/Homeworks/Homework13/Homework13/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Task
+{
+    public class Student : Person
+    {
+        public double GPA { get; set; }
+        public string GroupName { set; get; }
+
+        public Student(string name, string surname, int age, int phone, double gpa, string groupname) : base(name, surname, age, phone)
+        {
+            this.GPA = gpa;
+            this.GroupName = groupname;
+        }
+
+        public Student() : base()
+        {
+            GPA = 0.0;
+            GroupName = "Unklow";
+        }
+
+        public void Print()
+        {
+            base.Print();
+            Console.WriteLine("Средній бал {0}, Назва групи {1}", GPA, GroupName);
+        }
+    }
+
+}
---
+++ src/Homeworks/Homework13/MainClass/Main.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+
+namespace Task
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.InputEncoding = Encoding.UTF8;
+            AcademyGroup academyGroup = new AcademyGroup();
+            while (true)
+            {
+                Console.WriteLine("1. Add 2. Remove 3. Edit 4. Print 5. Save 6. Load 7. Search 9. Exit");
+                int select = int.Parse(Console.ReadLine());
+
+                switch (select)
+                {
+                    case 1:
+                        Console.WriteLine("Введіть Імя, Фамілію, вік, телефон, средній бал, Назву групи");
+                        string name = Console.ReadLine();
+                        string surname = Console.ReadLine();
+                        int age = int.Parse(Console.ReadLine());
+                        int phone = int.Parse(Console.ReadLine());
+                        double gpa = double.Parse(Console.ReadLine());
+                        string groupName = Console.ReadLine();
+                        academyGroup.Add(new Student(name, surname, age, phone, gpa, groupName));
+                        break;
+                    case 2:
+                        academyGroup.Remove();
+                        break;
+                    case 3:
+                        academyGroup.Edit();
+                        break;
+                    case 4:
+                        academyGroup.Print();
+                        break;
+                    case 5:
+                        academyGroup.Save();
+                        break;
+                    case 6:
+                        academyGroup.Load();
+                        break;
+                    case 7:
+                        academyGroup.Search();
+                        break;
+                    case 9:
+                        return;
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+
+            }
+        }
+    }
+}
